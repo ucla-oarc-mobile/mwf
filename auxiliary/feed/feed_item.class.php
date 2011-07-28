b8d93fafba0e64fb1b5fe48e3364e1c3f797e1a9
Nathan Emerson <emerson.nate@gmail.com>
2011-07-28 00:19:58 -0700

    Added PHPDoc to Feed API

--- auxiliary/feed/feed_item.class.php
+++ auxiliary/feed/feed_item.class.php
@@ -1,17 +1,61 @@
 <?php
 
+/**
+ * A class that represents an item in a Feed (generally an article). Provides
+ * accessors for item attributes as well as a method to build a request URL
+ * for a given item.
+ *
+ * @author ebollens
+ * @version 20110727
+ */
+
 class Feed_Item
 {
+    /**
+     * The item title
+     * 
+     * @var string 
+     */
     private $_title;
+    
+    /**
+     * The item link
+     * 
+     * @var string 
+     */
     private $_link;
+    
+    /**
+     * The item description
+     * 
+     * @var string 
+     */
     private $_description;
+    
+    /**
+     * The feed the item belongs to
+     * 
+     * @var Feed 
+     */
     private $_feed;
+    
+    /**
+     * The author of the item
+     * 
+     * @var string 
+     */
     private $_author;
 
+    /**
+     * The constructor stores feed item attributes as properties.
+     * 
+     * @param type $feed the feed the new item belongs to
+     * @param SimplePie_Item $item SimplePie_Item that contains information
+     * about the item
+     */
     public function __construct($feed, $item)
     {
         $this->_feed = $feed;
-        $this->_name = $item->get_title();
         $this->_title = $item->get_title();
         $this->_link = $item->get_permalink();
         $this->_description = $item->get_content();
@@ -21,41 +65,83 @@ class Feed_Item
         $this->_author = empty($author_name) ? $author->get_email() : $author_name;
     }
 
+    /**
+     * Accessor method to get the Feed the item belongs to
+     * 
+     * @return Feed feed the item belongs to
+     */
     public function get_feed()
     {
         return $this->_feed;
     }
 
+    /**
+     * Accessor method for the feed item name (alias for title)
+     * 
+     * @return string 
+     */
     public function get_name()
     {
-        return $this->_name;
+        return $this->_title;
     }
 
+    /**
+     * Accessor method for the feed item title
+     * 
+     * @return string 
+     */
     public function get_title()
     {
         return $this->_title;
     }
 
+    /**
+     * Accessor method for the feed item link
+     * 
+     * @return string 
+     */
     public function get_link()
     {
         return $this->_link;
     }
 
+    /**
+     * Accessor method for the feed item date
+     * 
+     * @return string 
+     */
     public function get_date()
     {
         return $this->_date;
     }
 
+    /**
+     * Accessor method for the feed item author
+     * 
+     * @return string 
+     */
     public function get_author()
     {
         return $this->_author;
     }
 
+    /**
+     * Accessor method for the feed item description
+     * 
+     * @return string 
+     */
     public function get_description()
     {
         return $this->_description;
     }
 
+    /**
+     * Accessor method to get a short description, which is simply the item
+     * description truncated to $n characters.
+     * 
+     * @param $n number of characters to truncate description to
+     * @return string|false the truncated description or false if not available
+     */
     public function get_short_description($n = 100)
     {
         if(!$this->_description)
@@ -78,12 +164,26 @@ class Feed_Item
         return $short_desc;
     }
 
+    /**
+     * Produces a URL for viewing a feed item. If a salt is provided, creates
+     * a signature and appends it to the URL.
+     * 
+     * @param string $salt a salt for verifying the page
+     * @return string the desired URL
+     */
     public function get_page($salt = null)
     {
         $path = 'item.php?name='.urlencode($this->get_feed()->get_name()).'&path='.urlencode($this->get_feed()->get_path()).'&article='.urlencode($this->get_title());
         return $salt ? $path.'&signature='.md5($salt.$this->get_feed()->get_name().$this->get_feed()->get_path().$this->get_title()) : $path;
     }
 
+    /**
+     * Verifies a signature based on the given salt.
+     * 
+     * @param type $signature signature to verify
+     * @param type $salt salt to use when verifying
+     * @return bool
+     */
     public function verify_page($signature, $salt)
     {
         return $signature == md5($salt.$this->get_feed()->get_name().$this->get_feed()->get_path().$this->get_title());
