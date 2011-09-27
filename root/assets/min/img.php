3a1a1a878c81e2f0e674fe8624fa87c32a1f687c
Rich Trott <rtrott@gmail.com>
2011-09-26 20:45:35 -0700

    Log problem before exiting

--- root/assets/min/img.php
+++ root/assets/min/img.php
@@ -21,8 +21,11 @@
 /**
  * If no img URL is provided, exit
  */
-if (! isset($_GET['img'])) exit;
-
+if (! isset($_GET['img'])) {
+    error_log('MWF Notice: Required URL parameter "img" not provided to ' . $_SERVER['PHP_SELF'],0);
+    exit(1);
+}
+    
 /**
  * Require necessary libraries. 
  */
