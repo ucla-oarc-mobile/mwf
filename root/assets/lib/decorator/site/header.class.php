d1f81b26f41c9b4f8808e7e778a9b04b750d3cbe
Eric Bollens <ebollens@ucla.edu>
2011-09-29 18:47:21 -0700

    Fix ternary operator precendence in some versions of PHP [MWF-218].

--- root/assets/lib/decorator/site/header.class.php
+++ root/assets/lib/decorator/site/header.class.php
@@ -59,7 +59,7 @@ class Header_Site_Decorator extends Tag_HTML_Decorator
                                   'alt'=>Config::get('global', 'header_home_button_alt'));
 
         $image = HTML_Decorator::tag('img', false, HTTPS::is_https() ? HTTPS::convert_path($this->_image) : $this->_image)->render();
-        $home_button = HTML_Decorator::tag('a', $image, array('href'=>HTTPS::is_https() ? HTTPS::convert_path(Config::get('global', 'site_url')) : Config::get('global', 'site_url')))->render();
+        $home_button = HTML_Decorator::tag('a', $image, array('href'=>(HTTPS::is_https() ? HTTPS::convert_path(Config::get('global', 'site_url')) : Config::get('global', 'site_url'))))->render();
 
         if($this->_title_path)
             $title = $this->_title ? HTML_Decorator::tag('a', $this->_title, array('href'=>$this->_title_path)) : false;
