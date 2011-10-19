20279652e969e74bdb3b068d1e6ac87298e3bb1d
Rich Trott <rtrott@gmail.com>
2011-10-18 22:27:26 -0400

    caniuse.com reports that no current versions of iOS or Android support WebGL. Total bummer!

--- root/assets/test/js_unit/core/capability.js
+++ root/assets/test/js_unit/core/capability.js
@@ -244,7 +244,7 @@ test("mwf.capability.webgl()", function()
     expect(2);
     var webgl = mwf.capability.webgl();
     equal(typeof webgl, 'boolean', 'mwf.capability.webgl() should return a boolean');
-    equal(webgl, true, 'browser supports WebGL');
+    equal(webgl, false, 'mobile browser does not support WebGL');
 });
 
 test("mwf.capability.websockets()", function()
