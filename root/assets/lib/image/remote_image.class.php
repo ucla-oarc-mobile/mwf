2712f1a4b0b9f5482f8eb70e17f349904f413baf
Rich Trott <rtrott@gmail.com>
2011-11-07 15:23:57 -0800

    Issue #13: Check probable memory usage before loading a remote image

--- root/assets/lib/image/remote_image.class.php
+++ root/assets/lib/image/remote_image.class.php
@@ -10,17 +10,17 @@
  * @author trott
  * @copyright Copyright (c) 2010-11 UC Regents
  * @license http://mwf.ucla.edu/license
- * @version 20110929
+ * @version 20111107
  *
  * @uses Image
  *
  * @todo Comments
  * @todo Refactor
  */
-require_once(dirname(dirname(__FILE__)).'/image.class.php');
+require_once(dirname(dirname(__FILE__)) . '/image.class.php');
 
 class Remote_Image extends Image {
-    
+
     private $_image_gd = null;
 
     protected function &get_gd_image() {
@@ -32,22 +32,22 @@ class Remote_Image extends Image {
             $this->_image_gd = false;
             return $this->_image_gd;
         }
-        
+
         $unlink_path = FALSE;
         $path = $this->get_cache_filename();
-        if (! file_exists($path)) {
+        if (!file_exists($path)) {
             $path = tempnam(sys_get_temp_dir(), 'mwf');
             // Encode the URL
             $image_url = preg_replace_callback('#://([^/]+)/([^?]+)#', function ($match) {
-                    return '://' . $match[1] . '/' . join('/', array_map('rawurlencode', explode('/', $match[2])));
-                }, $image_path);
+                        return '://' . $match[1] . '/' . join('/', array_map('rawurlencode', explode('/', $match[2])));
+                    }, $image_path);
             if (ini_get('allow_url_fopen')) {
                 file_put_contents($path, file_get_contents($image_url, FALSE, NULL, -1, 9999999));
             } else {
                 $ch = curl_init($image_url);
                 $fh = fopen($path, 'wb');
                 curl_setopt($ch, CURLOPT_FILE, $fh);
-                curl_setopt($ch, CURLOPT_HEADER,0);
+                curl_setopt($ch, CURLOPT_HEADER, 0);
                 curl_setopt($ch, CURLOPT_FAILONERROR, 1);
                 curl_setopt($ch, CURLOPT_TIMEOUT, 3);
                 curl_exec($ch);
@@ -57,37 +57,45 @@ class Remote_Image extends Image {
             $unlink_path = TRUE;
         }
 
-        $ext = image_type_to_extension(exif_imagetype($path));
+        if ($this->check_memory($path)) {
+            error_log('MWF Notice: Image too large to process: ' . $image_path);
+            $this->_image_gd = false;
+            $this->_image_ext = false;
+        } else {
 
-        switch ($ext) {
-            case '.jpg':
-            case '.jpeg':
-                $this->_image_gd = imagecreatefromjpeg($path);
-                $this->_image_ext = 'jpeg';
-                break;
-            case '.gif':
-                $this->_image_gd = imagecreatefromgif($path);
-                $this->_image_ext = 'gif';
-                break;
-            case '.png':
-                $this->_image_gd = imagecreatefrompng($path);
-                $this->_image_ext = 'png';
-                break;
-            default:
-                $this->_image_gd = false;
-                $this->_image_ext = false;
-                break;
+            $ext = image_type_to_extension(exif_imagetype($path));
+
+            switch ($ext) {
+                case '.jpg':
+                case '.jpeg':
+                    $this->_image_gd = imagecreatefromjpeg($path);
+                    $this->_image_ext = 'jpeg';
+                    break;
+                case '.gif':
+                    $this->_image_gd = imagecreatefromgif($path);
+                    $this->_image_ext = 'gif';
+                    break;
+                case '.png':
+                    $this->_image_gd = imagecreatefrompng($path);
+                    $this->_image_ext = 'png';
+                    break;
+                default:
+                    $this->_image_gd = false;
+                    $this->_image_ext = false;
+                    break;
+            }
         }
         if ($unlink_path) {
             unlink($path);
         }
         return $this->_image_gd;
     }
-    
+
     protected function get_gd_extension() {
         $this->get_gd_image();
         return $this->_image_ext;
     }
+
 }
 
 ?>
