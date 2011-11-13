8c5da6cab3687e5e4a0595ca94f757339d44177f
Zorayr Khalapyan <zkhalapyan@gmail.com>
2011-11-12 16:50:48 -0800

    Added append, prepend, and remove functionality to the decorator class - these are functions that take into account first/last class markers.

--- root/assets/js/decorators/content.js
+++ root/assets/js/decorators/content.js
@@ -3,36 +3,105 @@ if(typeof mwf == "undefined")
     var mwf = function(){};
 }
 
-mwf.decorator.Content = function()
+mwf.decorator.Content = function(title, text)
 {
+    
+    var firstMarker = "content-first";
+    var lastMarker  = "content-last";
+    
     var content = document.createElement("div");
     
     
-    content.setTitle = function(title)
+    //If an initial title was specified, then set this conent's title.
+    if(mwf.decorator.isSet(title))
     {
-        this.appendChild(mwf.decorator.ContentTitle(title));
+        this.setTitle(title);
     }
     
+    
+    //If initial text is specified, then add it to the content as a text block.
+    if(mwf.decorator.isSet(text))
+    {
+        this.addTextBlock(text);
+    }
+    
+    /**  
+     * Sets the title of this content. If the specified title is null or 
+     * undefined, then the content's title, if it exists, will be removed.
+     * 
+     * @param title The title of the content to set.
+     */
+    content.setTitle = function(title)
+    {
+        if(!mwf.decorator.isSet(title) && mwf.decorator.isSet(this.contentTitle))
+        {
+            this.removeChild(this.contentTitle);
+            this.contentTitle = null;
+        }
+        else
+        {
+            mwf.decorator.prepend(this, mwf.decorator.ContentTitle(title), firstMarker, lastMarker);
+        }
+        
+        
+    }
+    
+    content.getTitle = function()
+    {
+        return (mwf.decorator.isSet(this.contentTitle))? this.contenTitle:null;
+    }
+    
+    
+    
+    content.addItem = function(contentItem)
+    {
+        content.appendChild(contentItem);
+        
+    }
+    
+    content.addButton = function(labe, url, callback)
+    {
+        
+    }
+    
+    content.addTextBlock = function(text)
+    {
+        
+    }
+    
+    content.setPadded = function(isPadded)
+    {
+        mwf.decorator.toggleClass(isPadded, this, "content-padded");
+        return this;
+    }
+    
+    content.setFull = function(isFull)
+    {
+        mwf.decorator.toggleClass(isFull, this, "content-full");
+        return this;
+    }
+    
+    //Set defaults.
+    content.setFull(true);
+    content.setPadded(true);
     
     return content;
 }
 
-mwf.decorator.ContentTitle = function(title)
+mwf.decorator.ContentTitle = function(title, isH4)
 {
-    var contentTitle = document.createElement('h1');
+    var contentTitle = document.createElement((isH4)? 'h4' : 'h1');
     
     contentTitle.innerHTML = title;
     
     contentTitle.setLight = function(isLight)
     {
-        
+        mwf.decorator.toggleClass(isLight, this, "light");
+        return this;
     }
     
-    contentTitle.isFull = function(isFull)
-    {
-        
-    }
-    
+    //Set defaults.
+    contentTitle.setLight(false);
     
     return contentTitle;
     
--- root/assets/js/decorators/decorator.js
+++ root/assets/js/decorators/decorator.js
@@ -18,12 +18,12 @@ mwf.decorator.isSet = function(variable)
 
 mwf.decorator.toggleClass = function(set, obj, className)
 {
+    
     //If the specified object is undefined, then return false.
     if(typeof obj == "undefined")
     {
         return;
     }
-    
     
     //If the className property of the object is undefined, then define it to
     //empty string.
@@ -47,6 +47,7 @@ mwf.decorator.toggleClass = function(set, obj, className)
     //Unset class.
     else
     {
+        //Replace the class name with empty string.
         obj.className = obj.className.replace(className, "");   
     }
     
@@ -56,6 +57,93 @@ mwf.decorator.toggleClass = function(set, obj, className)
     {
         obj.className = null;
     }
-}
     
+    //In case className is not empty, then trim any whitespace either from the 
+    //left side or the right side.
+    else
+    {
+        obj.className = obj.className.replace(/^\s+|\s+$/g,"");
+    }
+}
 
+mwf.decorator.setClass = function(obj, className)
+{
+    mwf.decorator.toggleClass(true, obj, className);
+}
+
+mwf.decorator.unsetClass = function(obj, className)
+{
+    mwf.decorator.toggleClass(false, obj, className);
+}
+
+mwf.decorator.prepend = function(container, element, firstMarker, lastMarker)
+{
+    //Add the last marker to the element to be prepended to the container.
+    mwf.decorator.setClass(element, firstMarker);
+        
+    //If the container does not have any children, then the element will include
+    //both first and last markers.
+    if(container.children.length == 0)
+    {
+        mwf.decorator.setClass(element, lastMarker);
+    } 
+    
+    //Handle the case where there is at least one element in the container's 
+    //list.
+    else 
+    {        
+        //Remove the first marker from the current first element.
+        mwf.decorator.unsetClass(container.firstChild, firstMarker);   
+    }
+    
+    //Append the element to the front of the container's children. I am assuming
+    //here that if firstChild is undefined, then appendChild will still append
+    //the element at the end.
+    container.insertBefore(element, container.firstChild);
+}
+
+mwf.decorator.append = function(container, element, firstMarker, lastMarker)
+{
+    
+    //Add the last marker to the element to be appended to the container.
+    mwf.decorator.setClass(element, lastMarker);
+        
+    //If the container does not have any children, then the element will include
+    //both first and last markers.
+    if(container.children.length == 0)
+    {
+        mwf.decorator.setClass(element, firstMarker);
+    } 
+    //Handle the case where there is at least one element in the container's 
+    //list.
+    else 
+    {        
+        //Remove the last marker from the current last element.
+        mwf.decorator.unsetClass(container.lastChild, lastMarker);   
+    }
+    
+    //Append the element to the end of the container's children.
+    container.appendChild(element);
+    
+}
+
+mwf.decorator.remove = function(container, element, firstMarker, lastMarker)
+{
+    //Unset both types of markers from the element to be removed.
+    mwf.decorator.unsetClass(element, firstMarker);
+    mwf.decorator.unsetClass(element, lastMarker);
+    
+    //Remove the child from the container. 
+    container.removeChild(element);
+    
+    //In case the container has any other elements, then add firstMarker to the
+    //first child and lastMarker to the last child of the container.
+    if(container.children.length >= 1)
+    {
+        mwf.decorator.setClass(container.firstChild, firstMarker);
+        mwf.decorator.setClass(container.lastChild, lastMarker);
+         
+    }
+    
+    
+}
