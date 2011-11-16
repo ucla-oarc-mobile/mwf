975cda22e3819b68160e4754627416276adcc69a
Rich Trott <rtrott@gmail.com>
2011-11-16 13:52:53 -0800

    PHP unit tests for Classification class

---
+++ test/php_unit/root/assets/lib/ClassificationTest.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * Test class for Classification.
+ * 
+ * @author trott
+ * @copyright Copyright (c) 2010-11 UC Regents
+ * @license http://mwf.ucla.edu/license
+ * @version 20111116
+ *
+ * @uses PHPUnit_Framework_TestCase
+ * @uses Classification
+ */
+class ClassificationTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp() {
+        $_SERVER['SERVER_PORT']="80";
+        $_SERVER['SERVER_NAME']='localhost';
+        $_COOKIE=array();
+    }
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown() {
+        
+    }
+
+    /**
+     * @test
+     * @runInSeparateProcess
+     */
+    public function isFull_isFull_True() {
+        $_COOKIE['mwf_classification']='{"mobile":false,"basic":true,"standard":true,"full":true}';
+        require_once dirname(__FILE__) . '/../../../../../root/assets/lib/classification.class.php';
+        $this->assertTrue(Classification::is_full());
+    }
+
+    /**
+     * @test
+     * @runInSeparateProcess
+     */
+    public function isFull_notFull_False() {
+        $_COOKIE['mwf_classification']='{"mobile":false,"basic":true,"standard":true,"full":false}';
+        require_once dirname(__FILE__) . '/../../../../../root/assets/lib/classification.class.php';
+        $this->assertFalse(Classification::is_full());
+    }
+ 
+    /**
+     * @test
+     * @runInSeparateProcess
+     */
+    public function isStandard_isStandard_True() {
+        $_COOKIE['mwf_classification']='{"mobile":false,"basic":true,"standard":true,"full":true}';
+        require_once dirname(__FILE__) . '/../../../../../root/assets/lib/classification.class.php';
+        $this->assertTrue(Classification::is_standard());
+    }
+
+    /**
+     * @test
+     * @runInSeparateProcess
+     */
+    public function isStandard_notStandard_False() {
+        $_COOKIE['mwf_classification']='{"mobile":false,"basic":true,"standard":false,"full":false}';
+        require_once dirname(__FILE__) . '/../../../../../root/assets/lib/classification.class.php';
+        $this->assertFalse(Classification::is_standard());
+    }
+
+    /**
+     * @test
+     * @runInSeparateProcess
+     */
+    public function isBasic_isBasic_True() {
+        $_COOKIE['mwf_classification']='{"mobile":false,"basic":true,"standard":true,"full":true}';
+        require_once dirname(__FILE__) . '/../../../../../root/assets/lib/classification.class.php';
+        $this->assertTrue(Classification::is_basic());
+    }
+
+    /**
+     * @test
+     * @runInSeparateProcess
+     */
+    public function isMobile_isMobile_True() {
+        $_COOKIE['mwf_classification']='{"mobile":true,"basic":true,"standard":true,"full":true}';
+        require_once dirname(__FILE__) . '/../../../../../root/assets/lib/classification.class.php';
+        $this->assertTrue(Classification::is_mobile());
+    }
+
+    /**
+     * @test
+     * @runInSeparateProcess
+     */
+    public function isMobile_notMobile_False() {
+        $_COOKIE['mwf_classification']='{"mobile":false,"basic":true,"standard":false,"full":false}';
+        require_once dirname(__FILE__) . '/../../../../../root/assets/lib/classification.class.php';
+        $this->assertFalse(Classification::is_standard());
+    }
+    
+//    /**
+//     * @test
+//     */
+//    public function isNative_isNative_True() {
+//        // Remove the following lines when you implement this test.
+//        $this->markTestIncomplete(
+//                'This test has not been implemented yet.'
+//        );
+//    }
+//
+//    /**
+//     * @test
+//     */
+//    public function isNative_notNative_False() {
+//        // Remove the following lines when you implement this test.
+//        $this->markTestIncomplete(
+//                'This test has not been implemented yet.'
+//        );
+//    }
+    
+}
+
+?>
