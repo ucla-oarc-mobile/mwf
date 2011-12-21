639f6cbfe3f015d4a5397149cd3b7f461c1c6089
Rich Trott <rtrott@gmail.com>
2011-12-21 15:49:31 -0800

    Revert "Merge branch 'develop-1.2' into develop"
    This reverts commit 98a8662d1cc616dd46d54725ad5ca31e855fd252, reversing
    changes made to 11839b8e3d289f45361124ce09a755288f8ba6f9.

--- root/index.php
+++ root/index.php
@@ -32,40 +32,32 @@
  * @link /config/global.php
  * @link assets/redirect/unset_override.php
  */
-
 /**
  * Require necessary libraries.
  */
-
-require_once(dirname(__FILE__).'/assets/config.php');
-require_once(dirname(__FILE__).'/assets/lib/decorator.class.php');
-require_once(dirname(__FILE__).'/assets/redirect/unset_override.php');
+require_once(dirname(__FILE__) . '/assets/config.php');
+require_once(dirname(__FILE__) . '/assets/lib/decorator.class.php');
+require_once(dirname(__FILE__) . '/assets/redirect/unset_override.php');
 
 /**
  * Ensure that site_url and site_asset_url have been set.
  */
-
-if(!Config::get('global', 'site_url') || !Config::get('global', 'site_assets_url'))
-	die('<h1>Fatal Error</h1><p>The configuration settings {global::site_url} and {global::site_asset_url} must be defined in '.dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'global.php</p>');
+if (!Config::get('global', 'site_url') || !Config::get('global', 'site_assets_url'))
+    die('<h1>Fatal Error</h1><p>The configuration settings {global::site_url} and {global::site_asset_url} must be defined in ' . dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'global.php</p>');
 
 /**
  * Get the menu from {'frontpage':'menu'} defined in config/frontpage.php.
  */
-
 $menu = Config::get('frontpage', 'menu');
 
 /**
  * Handle differences between a subsection and the top-level menu, using key
  * 'default' if on the front page or otherwise the $_GET['s'] parameter.
  */
-
-if(isset($_GET['s']) && isset($menu[$_GET['s']]))
-{
+if (isset($_GET['s']) && isset($menu[$_GET['s']])) {
     $menu_items = $menu[$_GET['s']];
     $main_menu = false;
-}
-else
-{
+} else {
     $menu_items = $menu['default'];
     $main_menu = true;
 }
@@ -78,7 +70,7 @@ echo HTML_Decorator::html_start()->render();
 
 echo Site_Decorator::head()->set_title(Config::get('global', 'title_text'))->render();
 
-echo HTML_Decorator::body_start($main_menu ? array('class'=>'front-page') : array())->render();
+echo HTML_Decorator::body_start($main_menu ? array('class' => 'front-page') : array())->render();
 
 /*
  * Header
@@ -102,14 +94,13 @@ for($i = 0; $i < count($menu_items); $i++)
 {
     $menu_item = $menu_items[$i];
 
-    if(isset($menu_item['restriction']))
-    {
+    if (isset($menu_item['restriction'])) {
         $function = $menu_item['restriction'];
-        if(!User_Agent::$function())
+        if (!User_Agent::$function())
             continue;
     }
 
-    $menu->add_item($menu_item['name'],$menu_item['url'],isset($menu_item['id'])?array('id'=>$menu_item['id']):array());
+    $menu->add_item($menu_item['name'], $menu_item['url'], isset($menu_item['id']) ? array('id' => $menu_item['id']) : array());
 }
 
 echo $menu->render();
@@ -117,31 +108,21 @@ echo $menu->render();
 /**
  * Back button
  */
-
-if(!$main_menu)
+if (!$main_menu)
     echo Site_Decorator::button_full()
-                ->set_padded()
-                ->add_option(Config::get('global', 'back_to_home_text'), 'index.php')
-                ->render();
+            ->set_padded()
+            ->add_option(Config::get('global', 'back_to_home_text'), 'index.php')
+            ->render();
 
 /**
  * Footer
  */
 
-$footer = Site_Decorator::footer();
-
-if($full_site_url = Config::get('frontpage', 'full_site_url'))
-    $footer->set_full_site('Full Site', Config::get('frontpage', 'full_site_url'));
-
-if($help_site_url = Config::get('frontpage', 'help_site_url'))
-    $footer->set_help_site('Help', Config::get('frontpage', 'help_site_url'));
-
-echo $footer->render();
+echo Site_Decorator::default_footer()->render();
 
 /**
  * End page
  */
-
 echo HTML_Decorator::body_end()->render();
 
 echo HTML_Decorator::html_end()->render();
