85edce0997466a236a86afa38ab60a5ab6070550
Rich Trott <rtrott@gmail.com>
2011-12-28 21:49:56 -0800

    Keep cookie-nuking simple

--- root/assets/test/selenium/nuke_cookies.php
+++ root/assets/test/selenium/nuke_cookies.php
@@ -1,13 +1,9 @@
 <?php
 
-if (isset($_SERVER['HTTP_COOKIE'])) {
-    $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
-    foreach($cookies as $cookie) {
-        $parts = explode('=', $cookie);
-        $name = trim($parts[0]);
-        setcookie($name, '', time()-1000);
-        setcookie($name, '', time()-1000, '/');
-    }
+$past = time() - 3600;
+foreach ( $_COOKIE as $key => $value )
+{
+    setcookie( $key, FALSE, $past, '/' );
 }
 ?>
 <!doctype html>
