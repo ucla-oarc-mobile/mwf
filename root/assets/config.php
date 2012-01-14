7a2d822a14270c0ecdd13d1bf97ed19fc57aac6d
Ike Lin <ilin@ucsd.edu>
2012-01-14 00:13:02 -0800

    remove site specific param

--- root/assets/config.php
+++ root/assets/config.php
@@ -18,7 +18,4 @@
 
 require_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'config.class.php');
 
-Config::set('global', 'site_url',                   '//' . $_SERVER['SERVER_ADDR'] . '/mwf');
-Config::set('global', 'site_assets_url',            Config::get('global', 'site_url') . '/assets');
-
 ?>
